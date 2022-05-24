6aa42113613756e4662e457b7ad91aed93fd3500
Pablo Martínez Roca <pablo.martinezroca@d-mice.com>
2022-05-23 23:00:01 -0300

    Primer Commit

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { PerfilComponent } from './componentes/perfil/perfil.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { PortfolioComponent } from './componentes/portfolio/portfolio.component';
+import { PieDePaginaComponent } from './componentes/pie-de-pagina/pie-de-pagina.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EncabezadoComponent,
+    PerfilComponent,
+    AcercaDeComponent,
+    EducacionComponent,
+    PortfolioComponent,
+    PieDePaginaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
