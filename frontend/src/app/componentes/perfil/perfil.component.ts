5eb1ec203d15879d0b0035953ad8fb621499b6f3
Pablo Martínez Roca <pablo.martinezroca@d-mice.com>
2022-05-27 00:06:06 -0300

    Base de Datos

--- frontend/src/app/componentes/perfil/perfil.component.ts
+++ frontend/src/app/componentes/perfil/perfil.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Perfil } from 'src/app/entidades/perfil';
 import { DatosPerfilService } from 'src/app/servicios/datos-perfil.service';
 
 @Component({
@@ -14,22 +15,71 @@ export class PerfilComponent implements OnInit {
 
   form!: FormGroup;
 
-  constructor(private datosPerfil: DatosPerfilService, private formBuilder: FormBuilder) {}
+  constructor(
+    private datosPerfil: DatosPerfilService,
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit(): void {
     this.datosPerfil.cargarDatos().subscribe((data) => {
-      this.perfil = data['persona'];
+      this.perfil = data["perfil"];
+      console.log(this.perfil.nombre);
     });
 
     this.form = this.formBuilder.group({
       nombre: ['', [Validators.required]],
       apellido: ['', [Validators.required]],
-      fechaNacimiento: [''],
-      contactoEmail: ['', [Validators.required,Validators.email]],
-      url:['', [Validators.required,Validators.pattern('https?://.+')]]
-    })
+      fechaNacimiento: ['', Validators.required],
+      contactoEmail: ['', [Validators.required, Validators.email]],
+      url: ['', [Validators.required, Validators.pattern('https?://.+')]],
+    });
   }
 
+  guardarPerfil() {
+    if (this.form.valid) {
+      let nuevosDatosPerfil = new Perfil (
+        this.form.controls['nombre'].value,
+        this.form.controls['apellido'].value,
+        this.form.controls['fechaNacimiento'].value,
+        this.form.controls['contactoEmail'].value,
+        this.form.controls['url'].value,
+        this.perfil.titulosAcademicos
+      );
+      
+      this.datosPerfil.guardarDatos(nuevosDatosPerfil).subscribe(
+        (data) => {
+          alert('Los datos se modificaron con éxito.');
+
+          this.perfil = nuevosDatosPerfil;
+
+          this.form.reset();
+          document.getElementById('btnCerrarModalFormPerfil')?.click();
+        },
+        (error) => {
+          alert(
+            'Ha ocurrido un error al actualizar los datos. Por favor, reintente.<br />Si el problema persiste, contacte al administrador.'
+          );
+        }
+      );
+    } else {
+      this.form.markAllAsTouched();
+    }
+  }
+
+  cargarDatosActualesAlFormulario() {
+    this.form.get('nombre')?.setValue(this.perfil.nombre);
+    this.form.get('apellido')?.setValue(this.perfil.apellido);
+    this.form.get('fechaNacimiento')?.setValue(this.perfil.fechaNacimiento);
+    this.form.get('contactoEmail')?.setValue(this.perfil.contactoEmail);
+    this.form.get('url')?.setValue(this.perfil.url);
+  }
+
+  resetForm() {
+    // Limpia los mensajes de error
+    this.form.reset();
+  }
+
+  // Getters and Setters
   get nombre() {
     return this.form.get('nombre');
   }
@@ -49,17 +99,4 @@ export class PerfilComponent implements OnInit {
   get url() {
     return this.form.get('url');
   }
-
-  guardarPerfil() {
-    if (this.form.valid)
-    {
-      //llamar a un servicio para enviar los datos
-    }
-    else
-    {
-      this.form.markAllAsTouched();
-    }
-  }
 }
-
-
--- frontend/src/app/servicios/datos-perfil.service.ts
+++ frontend/src/app/servicios/datos-perfil.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Perfil } from '../entidades/perfil';
 
 @Injectable({
   providedIn: 'root',
@@ -9,6 +10,14 @@ export class DatosPerfilService {
   constructor(private http: HttpClient) {}
 
   cargarDatos(): Observable<any> {
-    return this.http.get('./assets/data/persona.json');
+    // Carga datos desde un JSON estático
+    // return this.http.get('./assets/data/perfil.json');
+
+    // Carga datos desde un JSON Server
+    return this.http.get('http://localhost:3000/db');
+  }
+
+  guardarDatos(perfil:Perfil): Observable<any> {
+    return this.http.post('http://localhost:3000/perfil', perfil);
   }
 }
